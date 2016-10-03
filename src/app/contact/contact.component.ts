76f9ac4fc1cb9fbdad13131b89f0391a4a98d7cc
Daniel <znowm4n@gmail.com>
2016-10-03 15:58:50 +0200

    Updated navigation links

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 // import * as sendgrid from 'sendgrid';
 
 @Component({
-  template: require('./contact.component.html')
+  templateUrl: './contact.component.html'
 })
 
 export class ContactComponent {
